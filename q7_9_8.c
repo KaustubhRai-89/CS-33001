4e490977865879bfed429d8f5fcf0a1ec711c139
KaustubhRai-89 <2330093@kiit.ac.in>
2023-08-15 17:25:20 +0530

    WAP to display word IIT using character *

---
+++ q7_9_8.c
@@ -0,0 +1,13 @@
+//WAP to display word "IIT" using character "*"
+#include <stdio.h>
+
+int main() {
+    printf(" *****   ***** ******* \n");
+    printf("   *       *      *  \n");
+    printf("   *       *      *  \n");
+    printf("   *       *      *  \n");
+    printf(" *****   *****    * \n");
+
+    return 0;
+}
+
