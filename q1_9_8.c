3c96ef88859b7db3c7b6c7ca7215baca086dacd9
Kaustubh <2330093@kiit.ac.in>
2023-08-15 16:07:15 +0530

    Initial commit

---
+++ q1_9_8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float firstNumber, secondNumber, answer;
+
+    // Input the numbers
+    printf("Enter the first number: ");
+    scanf("%f", &firstNumber);
+
+    printf("Enter the second number: ");
+    scanf("%f", &secondNumber);
+
+    // Subtract the numbers
+    answer=firstNumber-secondNumber;
+
+    // Display the result
+    printf("The result of %f and %f is = %f \n",firstNumber,secondNumber,answer);
+    return 0;
+}
+
+
