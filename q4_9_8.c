73b949d18a54c255f16509fa7e144eaec84bac5e
KaustubhRai-89 <2330093@kiit.ac.in>
2023-08-15 17:01:12 +0530

    WAP to swap the content of two variables without using third variable.

---
+++ q4_9_8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int firstNumber, secondNumber;
+
+    // Input the numbers
+    printf("Enter the first number: ");
+    scanf("%d", &firstNumber);
+
+    printf("Enter the second number: ");
+    scanf("%d", &secondNumber);
+
+    // Swapping without a third variable
+    firstNumber = firstNumber + secondNumber;
+    secondNumber = firstNumber - secondNumber;
+    firstNumber = firstNumber - secondNumber;
+
+    // Display the result
+    printf("After swapping first number = %d and second number = %d \n", firstNumber, secondNumber);
+    return 0;
+}
+
